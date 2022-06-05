4be0ef8622210321ed2c7c9bc58765b17b0a3ef2
Artimedias <brettroper22@gmail.com>
2022-06-05 06:11:15 -0700

    Testing

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'css/styles.css';
+
+import Triangle from './triangle.js';
+import { Trapazoid, Rectangle, Circle } from './shapes.js';
+
+
+//css
+
+$(document).ready(function() {
+    $('#formOne').submit(function(event) {
+      
+    });
+  });
