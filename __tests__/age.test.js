a817e223431fee8ec5144272ea794ec63f3d96a0
Artimedias <brettroper22@gmail.com>
2022-06-05 09:01:16 -0700

    Added test for remaining life

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -9,6 +9,10 @@ describe('Age', () => {
         expect(player.ageCalc(player.age)[2]).toEqual(11.16);
         expect(player.ageCalc(player.age)[3]).toEqual(33.84);
         expect(player.ageCalc(player.age)[4]).toEqual(213.48);
-        
+    });
+
+    test('should correctly return the users age on earth, mercury, and venus', () => {
+        const person = new Age(75);
+        expect(person.lifeLeft(person.age)[0]).toEqual(25);
     });
 });
