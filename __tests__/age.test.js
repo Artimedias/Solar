a2fa53b86044254d75723ef0de906e84c779803c
Artimedias <brettroper22@gmail.com>
2022-06-05 08:04:36 -0700

    Added testing for all planet ages

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -6,6 +6,9 @@ describe('Age', () => {
         const player = new Age(18);
         expect(player.ageCalc(player.age)[0]).toEqual(18);
         expect(player.ageCalc(player.age)[1]).toEqual(4.32);
-        expect(player.ageCalc(player.age)[1]).toEqual(11.16);
+        expect(player.ageCalc(player.age)[2]).toEqual(11.16);
+        expect(player.ageCalc(player.age)[3]).toEqual(33.84);
+        expect(player.ageCalc(player.age)[4]).toEqual(213.48);
+        
     });
 });
