7734bd65a2ba01797054aea15c4c28dc021a8aa2
Artimedias <brettroper22@gmail.com>
2022-06-12 09:26:14 -0700

    Converted classes from ES5 to ES6

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,9 +1,15 @@
 import Age from './../src/age.js';
 
-describe('Age', () => {
+describe('AgeCalc', () => {
+    let player;
+    let person;
 
+    beforeEach(() => {
+        player = new Age(18, "male");
+        person = new Age(75, "neither");
+      });
     test('should correctly return the users age on earth, mercury, and venus', () => {
-        const player = new Age(18);
+       
         expect(player.ageCalc(player.age)[0]).toEqual(18);
         expect(player.ageCalc(player.age)[1]).toEqual(4.32);
         expect(player.ageCalc(player.age)[2]).toEqual(11.16);
@@ -11,13 +17,24 @@ describe('Age', () => {
         expect(player.ageCalc(player.age)[4]).toEqual(213.48);
     });
 
-    test('should correctly return the users life expectancy on earth, mercury, and venus', () => {
-        const person = new Age(75, "neither");
-        expect(person.lifeLeft(person.age)[0]).toEqual(25);
-        expect(person.lifeLeft(person.age)[1]).toEqual(6);
-        expect(person.lifeLeft(person.age)[2]).toEqual(15.5);
-        expect(person.lifeLeft(person.age)[3]).toEqual(47);
-        expect(person.lifeLeft(person.age)[4]).toEqual(296.5);
+    
+});
+
+describe('lifeLeft', () => {
+    let player;
+    let person;
+
+    beforeEach(() => {
+        person = new Age(75, "neither");
+      });
+
+      test('should correctly return the users life expectancy on earth, mercury, and venus, assuming they are not male or female', () => {
+       
+        expect(person.lifeLeft(person.life)[0]).toEqual(25);
+        expect(person.lifeLeft(person.life)[1]).toEqual(6);
+        expect(person.lifeLeft(person.life)[2]).toEqual(15.5);
+        expect(person.lifeLeft(person.life)[3]).toEqual(47);
+        expect(person.lifeLeft(person.life)[4]).toEqual(296.5);
         
     });
 });
--- src/age.js
+++ src/age.js
@@ -1,34 +1,35 @@
-export default function Age(age) {
+export default class Age {
+  constructor(age, sex) {
   this.age = age;
+  this.sex = sex;
+  }
 
-}
 
-Age.prototype.ageCalc = function(age) 
-{
-  let ageArray = [age];
+
+  ageCalc() {
+  let ageArray = [this.age];
   const planets = [0.24, 0.62, 1.88, 11.86]
 
   for (let i = 0; i < planets.length; i++)
   {
-    ageArray.push(age * planets[i]);
+    ageArray.push(this.age * planets[i]);
     if((ageArray[i]).toString().length > 5)
     {
       ageArray[i] = ageArray[i].toFixed(2);
       ageArray[i] = parseFloat(ageArray[i]);
     }
   }
-  
   return ageArray;
-};
+}
 
-Age.prototype.lifeLeft = function(age, sex)
-{
+lifeLeft() {
   let life = [];
-  if (sex === "male")
+  let ageArray = [];
+  if (this.sex === "male")
   {
    life = [76.1, 18.26, 47.18, 143.07, 902.55]
   }
-  else if (sex === "female")
+  else if (this.sex === "female")
   {
  life = [81.1, 19.46, 50.28, 152.47, 961.87]
   }
@@ -37,12 +38,14 @@ Age.prototype.lifeLeft = function(age, sex)
    life = [100, 24, 62, 188, 1186]
   }
 
-  const person = new Age(age);
-  const ageArray = person.ageCalc(person.age)
+  
+  ageArray = this.ageCalc();
+  console.log(ageArray);
 
-  for (let i = 0; i < ageArray.length; i++)
+  for (let i = 0; i < 5; i++)
   {
      life[i] = (life[i] - ageArray[i]);
   }
   return life;
+}
 }
